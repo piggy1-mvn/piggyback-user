d8642502af0f536835644775e1bade84beaa09b2
Ashish Gupta <ashishddgr8@gmail.com>
2019-09-19 13:50:48 +0800

    api change from accepting list to just a string correct

--- src/main/java/com/incentives/piggyback/user/controller/UserController.java
+++ src/main/java/com/incentives/piggyback/user/controller/UserController.java
@@ -68,9 +68,9 @@ public class UserController {
     @GetMapping("/user/particular/interest")
     public ResponseEntity<List<Users>> getUserWithParticularInterest(
     		@RequestParam(value = "users", required = true) List<Long> users,
-    		@RequestParam(value = "interest", required = true) List<String> interests) {
+    		@RequestParam(value = "interest", required = true) String interest) {
         log.debug("User Service: Received GET request for getting all users with particular interest.");
-        return userService.getUserWithParticularInterest(users, interests);
+        return userService.getUserWithParticularInterest(users, interest);
     }
 
 }
