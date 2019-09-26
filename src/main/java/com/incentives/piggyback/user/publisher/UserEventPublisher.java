268e22d3973beb1f28330390897e22c6ca5dc09c
Ashish Gupta <ashishddgr8@gmail.com>
2019-09-26 14:57:26 +0800

    pub sub change

--- src/main/java/com/incentives/piggyback/user/publisher/UserEventPublisher.java
+++ src/main/java/com/incentives/piggyback/user/publisher/UserEventPublisher.java
@@ -1,9 +1,12 @@
 package com.incentives.piggyback.user.publisher;
 
 import com.incentives.piggyback.user.util.constants.Constant;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.MessageHandler;
@@ -11,10 +14,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserEventPublisher {
+	
+	@Autowired
+	private Environment env;
+	
     @Bean
     @ServiceActivator(inputChannel = "pubsubOutputChannel")
     public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
-        return new PubSubMessageHandler(pubsubTemplate, Constant.USER_PUBLISHER_TOPIC);
+        return new PubSubMessageHandler(pubsubTemplate, env.getProperty(Constant.USER_PUBLISHER_TOPIC));
     }
 
     @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
--- src/main/java/com/incentives/piggyback/user/util/constants/Constant.java
+++ src/main/java/com/incentives/piggyback/user/util/constants/Constant.java
@@ -1,13 +1,10 @@
 package com.incentives.piggyback.user.util.constants;
 
 public interface Constant {
-    String USER_PUBLISHER_TOPIC = "userEvents";
-    String USER_SOURCE_ID = "2";
-
-    String USER_CREATED_EVENT = "User Events Created";
-    String USER_UPDATED_EVENT = "User Events Updated";
-    String USER_DEACTIVATED_EVENT = "User Events Deactivated";
-
-     long JWT_TOKEN_VALIDITY = 5*60;
-
+	String USER_PUBLISHER_TOPIC = "user.publish.event";
+	String USER_SOURCE_ID = "2";
+	String USER_CREATED_EVENT = "User Events Created";
+	String USER_UPDATED_EVENT = "User Events Updated";
+	String USER_DEACTIVATED_EVENT = "User Events Deactivated";
+	long JWT_TOKEN_VALIDITY = 5*60;
 }
